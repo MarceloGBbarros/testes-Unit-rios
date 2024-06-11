88a9a3584db05b712f73bbd17b02a2a12b62c255
Marcelo Gonçalves <gbarros.marcelo@gmail.com>
2024-06-11 13:34:34 -0300

    teste simplificado de teste unitário

--- teste-avaliador.php
+++ teste-avaliador.php
@@ -6,7 +6,7 @@ use \App\Models\Usuario;
 use App\Http\Controllers\Avaliador;
 
 require_once "vendor/autoload.php";
-
+//arrumo tudo para o teste
 $leilao = new Leilao('fiat 147 0km');
 
 $maria = new Usuario('Maria');
@@ -15,10 +15,20 @@ $joao = new Usuario('João');
 $leilao->recebeLance(new Lance($joao,2000));
 $leilao->recebeLance(new Lance($maria,2500));
 
+//executo o código a ser testado
 $leiloeiro = new Avaliador($leilao);
 $leiloeiro->avalia($leilao);
 
+//verifico se a saída é a esperada
 $maiorValor = $leiloeiro->getMaiorValor();
 
-echo $maiorValor;
+//echo $maiorValor;
+
+$valorEsperado = 2500;
+
+if($valorEsperado == $maiorValor){
+    echo "Teste OK";
+}else{
+    echo "ERRO";
+}
 ?>
